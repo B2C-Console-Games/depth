dcbc1332ad88c373ccbb1173e0c1561eb41e1a91
Alan Angold <2552270+AlanAngold@users.noreply.github.com>
2024-04-07 16:57:21 -0400

    Added some comments

--- cpp/depth.cpp
+++ cpp/depth.cpp
@@ -36,6 +36,10 @@ std::vector<const char* > msg_location;
 std::vector<const char* > msg_wild;
 std::vector<const char* > msg_boom;
 
+/*
+ * Load a file of single line strings into a vector of strings.  Process embedded 'nl' characters
+ * into actual newline characters '\n' in the process.
+ */
 void load_file(const char* fn,std::vector< const char* >& list,const char nl='|')
 {
     char buf[1000];
@@ -54,11 +58,17 @@ void load_file(const char* fn,std::vector< const char* >& list,const char nl='|'
     }
 }
 
+/*
+ * From a string vector (loaded by load_file() ) pick one string at random.
+ */
 const char* pick_string(std::vector< const char* >& list)
 {
     return(list[rand()%(list.size())]);
 }
 
+/*
+ * Give the player an introductory message.
+ */
 void explain(int n)
 {
     const char* str=pick_string(msg_introduction);
@@ -67,6 +77,10 @@ void explain(int n)
     printf("\n\n\n");
 }
 
+/*
+ * Prodoce an Onomatopoeia word in banner ASCII Art text to display when
+ * submarine or player's destroyer are blown up.
+ */
 void boom()
 {
     const char* str=pick_string(msg_boom);
@@ -78,10 +92,12 @@ int main(void)
     int g, n;
     char ac;
     char ans[100];
-    
+ 
+    // TItle    
     printf("DEPTH CHARGE GAME\n");
     srand(time(NULL));
 
+    // Load all the text-message files.
     load_file("msg/introduction.txt",msg_introduction);    
     load_file("msg/victory.txt",msg_victory);    
     load_file("msg/failure.txt",msg_failure);    
@@ -89,6 +105,7 @@ int main(void)
     load_file("msg/boom.txt",msg_boom,'@');    
     load_file("msg/wild.txt",msg_wild);    
 
+    // Game loop
     bool play=true;
     while(play){
         // Get the search volume side-length
@@ -96,18 +113,21 @@ int main(void)
         fgets(ans,100,stdin);
         ans[strlen(ans)-1]=0;
         g=atoi(ans);
-        
+       
+        // Using the search volume side length determine optimal binary search O(n) 
         n = (int)(log(g) / log(2)) + 1;
         explain(n);
 
-        // Locate the submarine in our volume.
+        // Randomly locate the submarine in our volume.
         int sub_north = (int)(g * (rand() / (RAND_MAX + 1.0)));
         int sub_east = (int)(g * (rand() / (RAND_MAX + 1.0)));
         int sub_depth = (int)(g * (rand() / (RAND_MAX + 1.0)));
         //printf("sub_north=%d sub_east=%d sub_depth=%d\n",sub_north,sub_east,sub_depth);
 
+        // For each depth charge ...
         bool won=false;
         for(int d=1; (d<=n)&&(!won); d++) {
+            // Get the coordinates from the player.
             printf("\nDepth Charge: %d\n", d);
             printf("  Enter coordinates (N,E,D) : ");
             fgets(ans,100,stdin);
@@ -119,6 +139,7 @@ int main(void)
 
             int x,y,z;
             if(*ans == 0){
+                // User just hit <return> make the depth charge go wild.
                 const char* str=pick_string(msg_wild);
                 printf("    %s\n",str);
                 x = (int)(g * (rand() / (RAND_MAX + 1.0)));
@@ -127,6 +148,7 @@ int main(void)
                 printf("    Depth charge explodes at N,E,D=(%d,%d,%d)\n",x,y,z);
 
             }else{
+                // User entered something, try and interpret it as three numbers.
                 char* sx = strtok(ans,", \t");
                 char* sy = strtok(nullptr,", \t");
                 char* sz = strtok(nullptr,", \t");
@@ -139,7 +161,7 @@ int main(void)
             // Check for difference in location of depth-charge explosion and submarine.
             if (abs(x-sub_north) + abs(y-sub_east) + abs(z-sub_depth) == 0) {
 
-                // DC hit the submarine.
+                // Depth-charge hit the submarine and player wins
                 boom();
                 const char* str=pick_string(msg_victory);
                 printf("\n\n\n%s\n\n",str);
@@ -147,11 +169,12 @@ int main(void)
                 won=true;
 
             } else {
-
+                // Depth-charge missed submarine tell player by how much.
                 const char* ns="";
                 const char* ew="";
                 const char* ud="Depth OK";
 
+                // Only tell the player about coordinates that were wrong.
                 if(x<sub_north){
                     ns="South";
                 }else if(x>sub_north){
@@ -186,20 +209,25 @@ int main(void)
             }
         }
 
-        // Check for victory conditions.
+        // Check to see if the player lost.
         if(!won){
+            // Display the Onomatopoeia explosion message.
             boom();
 
+            // Display the failure message.
             const char* str=pick_string(msg_failure);
             printf("\n\n%s",str);
 
+            // Display the submarine location message.
             str=pick_string(msg_location); 
             printf(str,sub_north,sub_east,sub_depth,sub_depth*10);
 
+            // Prompt user for another game.
             printf("\n\n\nAnother game (Y OR N)");
             fgets(ans,100,stdin);
             ans[strlen(ans)-1]=0;
-            
+           
+            // If they replied with a string beginning with 'y' or 'Y' then continue. 
             if(toupper(ans[0])!='Y'){
                 printf("OK.  Hope you enjoyed yourself\n");
                 play=false;
